ed868bae894b02729fa8655e6a945cdf62e266a4
Yuta_Ushijima <register@yuta-u.com>
2018-05-04 16:58:34 +0000

    初期設定とHome.vue/Editor.vue作成まで

---
+++ src/Home.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div id="home">
+    <h1>{{msg}}</h1>
+    <button>Googleアカウントでログイン</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: "ぷちノートへようこそ"
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
