b11afe61545c8895b0d9c8b4a08968e562c2f4b3
Yuta_Ushijima <register@yuta-u.com>
2018-05-04 20:03:43 +0000

    Googleログインの実装/ログイン状態のチェックの実装

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,43 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class=""  v-if = "isLogin">
+      <Editor></Editor>
+    </div>
+    <div class=""  v-else>
+      <Home></Home>
+    </div>
+
   </div>
 </template>
 
 <script>
+import Home from './components/Home.vue';
+import Editor from './components/Editor.vue';
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'ぷちノートへようこそ！'
+      isLogin: false
     }
-  }
+  },
+  created: function() {
+    firebase.auth().onAuthStateChanged(user => {
+      console.log(user);
+      if(user) {
+        this.isLogin = true;
+      } else {
+        this.is_Login = false;
+      };
+    });
+  },
+  components: {
+    'Home': Home,
+    'Editor': Editor,
+  },
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
