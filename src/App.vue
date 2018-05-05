0bf0d0c3b38709200cbe3c1846b8ea1f72cc3c26
Yuta_Ushijima <register@yuta-u.com>
2018-05-05 20:08:04 +0000

    削除機能と保存機能の実装

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,41 @@
 <template>
-  <div id="app">
-      <Editor  v-if = "isLogin" ></Editor>
-      <!-- <Editor  v-if = "isLogin" :user="userData"></Editor> -->
-      <Home v-else ></Home>
-  </div>
+<div id="app">
+
+  <Editor v-if="isLogin" :user="userData"></Editor>
+  <Home v-else></Home>
+</div>
 </template>
 
 <script>
 import Home from './components/Home.vue';
 import Editor from './components/Editor.vue';
-
 export default {
   name: 'app',
-  data () {
+  data() {
     return {
-      isLogin: true,
-      // userData: null,
+      isLogin: false,
+      userData: null,
     }
   },
-  // created: function() {
-  //   firebase.auth().onAuthStateChanged(user => {
-  //     console.log(user);
-  //     if(user) {
-  //       this.isLogin = true;
-  //       // this.userData = user;
-  //     } else {
-  //       this.isLogin = false;
-  //       // this.userData = null;
-  //     };
-  //   });
-  // },
+  created: function() {
+    firebase.auth().onAuthStateChanged(user => {
+      console.log(user);
+      if (user) {
+        this.isLogin = true;
+        this.userData = user;
+      } else {
+        this.isLogin = false;
+        this.userData = null;
+      };
+    });
+  },
   components: {
     'Home': Home,
     'Editor': Editor,
   },
 }
 </script>
+
 
 <style lang="scss">
 
